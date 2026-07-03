731470f2e01287124a837d47a764460ef2651f77
fennaraOfficial <fennara.gamedev@gmail.com>
2026-07-03 17:16:06 +0530

    chore: bump version to 0.3.5

--- fennara-cpp/include/fennara/local_bridge.hpp
+++ fennara-cpp/include/fennara/local_bridge.hpp
@@ -21,7 +21,7 @@ protected:
     static void _bind_methods();
 
 public:
-    static constexpr const char *PLUGIN_VERSION = "0.3.4";
+    static constexpr const char *PLUGIN_VERSION = "0.3.5";
 
     void _ready() override;
     void _process(double delta) override;
